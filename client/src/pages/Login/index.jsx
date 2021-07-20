fda27a651b5a6702a4205dfdf6c59e818e0d611c
Gabriel Gonçalves Pereira <ggonpereira@gmail.com>
2021-07-20 00:35:16 -0300

    Removed unused imports

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext, useCallback, useEffect } from 'react'
+import React, { useState, useContext } from 'react'
 
 import { AuthContext } from '../../contexts/AuthContext';
 
