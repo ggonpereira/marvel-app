27d13a6a60d73e13262135e23171ff70c350278a
Gabriel Gonçalves Pereira <ggonpereira@gmail.com>
2021-07-20 18:47:22 -0300

    Input component placeholder changed

--- client/src/components/Input/index.jsx
+++ client/src/components/Input/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Input({ inputFunc, buttonFunc, type, id }) {
   return (
     <div className="input-group">
-      <input onChange={inputFunc} placeholder="Digite algo para buscar..." />
+      <input onChange={inputFunc} placeholder={`Busque pelo ID... Ex: ${type === "characters" ? "1009257" : "345"}`} />
       <button onClick={() => buttonFunc(type, id)}>Buscar</button>
     </div>
   )
